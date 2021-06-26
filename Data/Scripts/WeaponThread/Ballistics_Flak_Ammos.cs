e868bc41c762ed869971b986705f627faa353e61
Mike Dude <62247634+Blaylock1988@users.noreply.github.com>
2021-06-26 10:25:06 -0600

    Streamlined flak shrapnel

--- Data/Scripts/WeaponThread/Ballistics_Flak_Ammos.cs
+++ Data/Scripts/WeaponThread/Ballistics_Flak_Ammos.cs
@@ -115,32 +115,9 @@ namespace WeaponThread
                 AreaEffect = AntiSmart, // Disabled = do not use area effect at all, Explosive, Radiant, AntiSmart, JumpNullField, JumpNullField, EnergySinkField, AnchorField, EmpField, OffenseField, NavField, DotField.
                 Base = new AreaInfluence
                 {
-                    Radius = 50f, // the sphere of influence of area effects
-                    EffectStrength = 1f, // For ewar it applies this amount per pulse/hit, non-ewar applies this as damage per tick per entity in area of influence. For radiant 0 == use spillover from BaseDamage, otherwise use this value.
+                    Radius = 200f, // the sphere of influence of area effects
+                    EffectStrength = 2f, // For ewar it applies this amount per pulse/hit, non-ewar applies this as damage per tick per entity in area of influence. For radiant 0 == use spillover from BaseDamage, otherwise use this value.
                 },                
-				Pulse = new PulseDef // interval measured in game ticks (60 == 1 second), pulseChance chance (0 - 100) that an entity in field will be hit
-                {
-                    Interval = 1,
-                    PulseChance = 100,
-                    GrowTime = 0,
-                    HideModel = false,
-                    ShowParticle = false,
-                    Particle = new ParticleDef
-                    {
-                        Name = "", //ShipWelderArc
-                        ShrinkByDistance = false,
-                        Color = Color(red: 128, green: 0, blue: 0, alpha: 32),
-                        Offset = Vector(x: 0, y: -1, z: 0),
-                        Extras = new ParticleOptionDef
-                        {
-                            Loop = false,
-                            Restart = false,
-                            MaxDistance = 5000,
-                            MaxDuration = 1,
-                            Scale = 1,
-                        },
-                    },
-                },
                 Explosions = new ExplosionDef
                 {
                     NoVisuals = false,
@@ -362,45 +339,11 @@ namespace WeaponThread
         {
             AmmoMagazine = "Energy",
             AmmoRound = "AryxFlakAmmoWC_Shrapnel",
-            HybridRound = false, //AmmoMagazine based weapon with energy cost
-            EnergyCost = 0f, //(((EnergyCost * DefaultDamage) * ShotsPerSecond) * BarrelsPerShot) * ShotsPerBarrel
             BaseDamage = 1f,
             Mass = 100, // in kilograms
             Health = 0, // 0 = disabled, otherwise how much damage it can take from other trajectiles before dying.
             BackKickForce = 25f,
-            DecayPerShot = 0,
             HardPointUsable = false, // set to false if this is a shrapnel ammoType and you don't want the turret to be able to select it directly.
-
-            Shape = new ShapeDef //defines the collision shape of projectile, defaults line and visual Line Length if set to 0
-            {
-                Shape = LineShape,
-                Diameter = 1,
-            },
-            ObjectsHit = new ObjectsHitDef
-            {
-                MaxObjectsHit = 0, // 0 = disabled
-                CountBlocks = false, // counts gridBlocks and not just entities hit
-            },
-            Shrapnel = new ShrapnelDef
-            {
-                AmmoRound = "",
-                Fragments = 0,
-                Degrees = 0,
-                Reverse = false,
-                RandomizeDir = false, // randomzie between forward and backward directions
-            },
-            Pattern = new AmmoPatternDef
-            {
-                Ammos = new[] {
-                    "",
-                },
-                Enable = false,
-                TriggerChance = 1f,
-                Random = false,
-                RandomMin = 1,
-                RandomMax = 1,
-                SkipParent = false,
-            },
             DamageScales = new DamageScaleDef
             {
                 MaxIntegrity = 0f, // 0 = disabled, 1000 = any blocks with currently integrity above 1000 will be immune to damage.
@@ -409,15 +352,10 @@ namespace WeaponThread
                 HealthHitModifier = 1, // defaults to a value of 1, this setting modifies how much Health is subtracted from a projectile per hit (1 = per hit).
                 VoxelHitModifier = -1,
                 Characters = -1f,
-                FallOff = new FallOffDef
-                {
-                    Distance = 0f, // Distance at which max damage begins falling off.
-                    MinMultipler = 0f, // value from 0.0f to 1f where 0.1f would be a min damage of 10% of max damage.
-                },
                 Grids = new GridSizeDef
                 {
                     Large = 1f,
-                    Small = 1f,
+                    Small = 0.25f,
                 },
                 Armor = new ArmorDef
                 {
@@ -433,52 +371,12 @@ namespace WeaponThread
                     BypassModifier = -1f,
                 },
                 // first true/false (ignoreOthers) will cause projectiles to pass through all blocks that do not match the custom subtypeIds.
-                Custom = new CustomScalesDef
-                {
-                    IgnoreAllOthers = false,
-                    Types = new[]
-                    {
-                        new CustomBlocksDef
-                        {
-                            SubTypeId = "Test1",
-                            Modifier = -1f,
-                        },
-                        new CustomBlocksDef
-                        {
-                            SubTypeId = "Test2",
-                            Modifier = -1f,
-                        },
-                    },
-                },
             },
             AreaEffect = new AreaDamageDef
             {
                 AreaEffect = Explosive, // Disabled = do not use area effect at all, Explosive, Radiant, AntiSmart, JumpNullField, JumpNullField, EnergySinkField, AnchorField, EmpField, OffenseField, NavField, DotField.
                 AreaEffectDamage = 0, // 0 = use spillover from BaseDamage, otherwise use this value.
                 AreaEffectRadius = 0,
-                Pulse = new PulseDef // interval measured in game ticks (60 == 1 second), pulseChance chance (0 - 100) that an entity in field will be hit
-                {
-                    Interval = 0,
-                    PulseChance = 0,
-                    GrowTime = 0,
-                    HideModel = false,
-                    ShowParticle = false,
-                    Particle = new ParticleDef
-                    {
-                        Name = "", //ShipWelderArc
-                        ShrinkByDistance = false,
-                        Color = Color(red: 128, green: 0, blue: 0, alpha: 32),
-                        Offset = Vector(x: 0, y: -1, z: 0),
-                        Extras = new ParticleOptionDef
-                        {
-                            Loop = false,
-                            Restart = false,
-                            MaxDistance = 5000,
-                            MaxDuration = 1,
-                            Scale = 1,
-                        },
-                    },
-                },
                 Explosions = new ExplosionDef
                 {
                     NoVisuals = false,
@@ -496,37 +394,12 @@ namespace WeaponThread
                     DetonationDamage = 500,
                     DetonationRadius = 20,
                 },
-                EwarFields = new EwarFieldsDef
-                {
-                    Duration = 0,
-                    StackDuration = false,
-                    Depletable = false,
-                    MaxStacks = 0,
-                    TriggerRange = 0f,
-                },
-            },
-            Beams = new BeamDef
-            {
-                Enable = false,
-                VirtualBeams = false, // Only one hot beam, but with the effectiveness of the virtual beams combined (better performace)
-                ConvergeBeams = false, // When using virtual beams this option visually converges the beams to the location of the real beam.
-                RotateRealBeam = false, // The real (hot beam) is rotated between all virtual beams, instead of centered between them.
-                OneParticle = false, // Only spawn one particle hit per beam weapon.
             },
             Trajectory = new TrajectoryDef
             {
-                Guidance = None,
-                TargetLossDegree = 0f,
-                TargetLossTime = 0, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                 MaxLifeTime = 1, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
-                AccelPerSec = 0f,
-                DesiredSpeed = 0,
-                MaxTrajectory = 0f,
-                FieldTime = 0, // 0 is disabled, a value causes the projectile to come to rest, spawn a field and remain for a time (Measured in game ticks, 60 = 1 second)
-                GravityMultiplier = 0f, // Gravity multiplier, influences the trajectory of the projectile, value greater than 0 to enable.
-                SpeedVariance = Random(start: 0, end: 50), // subtracts value from DesiredSpeed
-                RangeVariance = Random(start: 0, end: 20), // subtracts value from MaxTrajectory
-                MaxTrajectoryTime = 0, // How long the weapon must fire before it reaches MaxTrajectory.
+                DesiredSpeed = 1,
+                MaxTrajectory = 1f,
             },
             AmmoGraphics = new GraphicDef
             {
@@ -535,8 +408,6 @@ namespace WeaponThread
                 ShieldHitDraw = true,
                 Lines = new LineDef
                 {
-                    ColorVariance = Random(start: 0f, end: 0f), // multiply the color by random values within range.
-                    WidthVariance = Random(start: 0f, end: 0f), // adds random value to default width (negatives shrinks width)
                     Tracer = new TracerBaseDef
                     {
                         Enable = true, //Must be true for Audio, Explosions, and Detonations to work
